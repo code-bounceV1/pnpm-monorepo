33073a3c3551d68a29d907c74044e0ccaa795f53
code-bounceV1 <akhil@tekenlight.com>
2026-03-10 15:01:05 +0530

    removing tests

--- packages/ui-mobile/vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vitest/config";
-import baseConfig from "../../vitest.config";
-
-export default defineConfig({
-  ...baseConfig,
-  test: {
-    ...baseConfig.test,
-  },
-});
